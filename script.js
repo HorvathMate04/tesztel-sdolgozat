51336e7e0f8109e9d3d88810e134b181f5a03b08
HorvathMate04 <culubpenguin@gmail.com>
2024-01-31 13:36:56 +0100

    minimum kiírása átírva maximum kiválasztásra, bug javítva, input kiolvasásnál int-é alakítás

--- script.js
+++ script.js
@@ -2,17 +2,19 @@ let list = [1,3,54,2];
 showData(list.join(";"))
 
 document.getElementById("btn").addEventListener("click", () => {
-    let data = document.getElementById("num-inp").value;
+    let data = Number(document.getElementById("num-inp").value);
     list.push(data)
     showData(list.join(";"));
 });
 
-document.getElementById("btn-min").addEventListener("click", () => {
-    let min = list[0]
+document.getElementById("btn-max").addEventListener("click", () => {
+    let max = list[0]
     list.forEach(item => {
-        if(item <min) min = item;
+        if(item > max) max = item;
+        console.log(item)
+        console.log(max)
     })
-    showData(min);
+    showData(max);
 });
 
 function showData(data){
