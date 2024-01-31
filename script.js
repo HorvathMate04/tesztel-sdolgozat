ac9117fcc6c52e60b0a3e5317cf2e444674117fc
HorvathMate04 <culubpenguin@gmail.com>
2024-01-31 13:18:35 +0100

    lista megjelenítése a weboldalon kész

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let list = [1,3,54,2];
+showData(list)
 
 document.getElementById("btn").addEventListener("click", () => {
     let data = document.getElementById("num-inp").value;
     list.push(data)
-    console.log(list)
+    showData();
 });
+
+function showData(){
+    document.getElementById("data").innerText = list.join(";");
+}
