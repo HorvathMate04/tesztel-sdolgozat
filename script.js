9f94794157e3ac29208213b348fdc3e9dfff819f
HorvathMate04 <culubpenguin@gmail.com>
2024-01-31 13:23:01 +0100

    minimum elem kiválasztása és megjelenítése kész

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 let list = [1,3,54,2];
-showData(list)
+showData(list.join(";"))
 
 document.getElementById("btn").addEventListener("click", () => {
     let data = document.getElementById("num-inp").value;
     list.push(data)
-    showData();
+    showData(list.join(";"));
 });
 
-function showData(){
-    document.getElementById("data").innerText = list.join(";");
+document.getElementById("btn-min").addEventListener("click", () => {
+    showData(list.sort()[0]);
+});
+
+function showData(data){
+    document.getElementById("data").innerText = data;
 }
